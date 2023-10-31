d99106c1f652c96a671a96ed7e46e321f4e4ae96
cpplv <aliepins@inbox.lv>
2023-10-31 10:00:00 +0200

    Changed button position

--- WindowsFormsApp20231031/Form1.Designer.cs
+++ WindowsFormsApp20231031/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(338, 250);
+            this.button1.Location = new System.Drawing.Point(398, 198);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
